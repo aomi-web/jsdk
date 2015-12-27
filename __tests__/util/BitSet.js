2a5a7ddd8879c90a22c4994186196a875dbb1d9c
Sean <sean.snow@live.com>
2015-12-27 21:01:56 +0800

    test

---
+++ __tests__/util/BitSet.js
@@ -0,0 +1,13 @@
+/**
+ * @author Sean sean.snow@live.com
+ * @date 2015/12/27
+ */
+
+import BitSet from '../../src/util/BitSet';
+
+var b = new BitSet(64);
+
+b.set(11);
+b.set(41);
+
+console.log(b);
